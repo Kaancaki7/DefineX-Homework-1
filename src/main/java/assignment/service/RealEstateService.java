9fd24dce71017f133de57e8c408da44bba417ce7
kaan <kaancaki0@gmail.com>
2023-01-22 15:46:11 +0300

    Added the method to return the total price, the method to return the average squaremeter and the method to filter the houses to the service

---
+++ src/main/java/assignment/service/RealEstateService.java
@@ -0,0 +1,39 @@
+package assignment.service;
+
+import assignment.house.House;
+
+import java.util.ArrayList;
+import java.util.List;
+public class RealEstateService {
+
+    public static long totalPrice(List<House> houses) {
+
+        long totalPrice = 0;
+        for (House house : houses) {
+            totalPrice += house.getPrice();
+        }
+
+        return totalPrice;
+    }
+
+    public static long averageSquareMeter(List<House> houses) {
+
+        long totalSquareMeter = 0;
+        for (House house : houses) {
+            totalSquareMeter += house.getSquareMeter();
+        }
+        int numberOfHouses = houses.size();
+        return totalSquareMeter / numberOfHouses;
+    }
+
+    public static List<House> houseListFilteredByRoomAndLivingRoomCount(List<House> houses, int roomCount, int livingRoomCount) {
+
+        List<House> filteredHouses = new ArrayList<House>();
+        for (House house : houses) {
+            if (house.getNumberOfRooms() == roomCount && house.getNumberOfLivingRooms() == livingRoomCount) {
+                filteredHouses.add(house);
+            }
+        }
+        return filteredHouses;
+    }
+}
